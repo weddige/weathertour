57b921cd06a69581cc1ae9a5afd79c55db2ed59a
Konstantin Weddige <konstantin@skathi.net>
2018-12-29 19:23:16 +0100

    Add DWD data

--- dwd/client.py
+++ dwd/client.py
@@ -1,21 +1,48 @@
 import re
+import zipfile
+import datetime
+import urllib.request
+import os.path
+import logging
+import lxml.etree
 
 from dwd.station import Station
 
 
+logger = logging.getLogger(__name__)
+
+
 class DWDClient:
-    def __init__(self, base_url='https://opendata.dwd.de/'):
+    def __init__(self, source_url='https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_S_LATEST_240.kmz'):
         self._stations = []
+        self._source_url = source_url
+        self._data_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'MOSMIX.kmz'))
+
+    def refresh(self):
+        logger.info('Retrieve data...')
+        urllib.request.urlretrieve(self._source_url, self._data_path)
 
     @property
     def stations(self):
         if not self._stations:
-            with open('data/RR_Monatswerte_Beschreibung_Stationen.txt', 'r', encoding='latin-1') as f:
-                for line in f.readlines()[2:]:
-                    parts = re.split('\s+', line, 6)
-                    name, bundesland = parts[6].rsplit(maxsplit=1)
-                    name = name.strip()
-                    self._stations.append(Station(parts[0], float(parts[3]), float(parts[4]), float(parts[5]), name, bundesland))
+            logger.info('Load stations...')
+            with zipfile.ZipFile(self._data_path, 'r') as kmz_file:
+                filename = kmz_file.namelist()[0]
+                with kmz_file.open(filename) as kml_file:
+                    kml = lxml.etree.parse(kml_file)
+                    
+            node = kml.getroot().find('{http://www.opengis.net/kml/2.2}Document')
+            node = node.find('{http://www.opengis.net/kml/2.2}ExtendedData')
+            node = node.find('{https://opendata.dwd.de/weather/lib/pointforecast_dwd_extension_V1_0.xsd}ProductDefinition')
+            node = node.find('{https://opendata.dwd.de/weather/lib/pointforecast_dwd_extension_V1_0.xsd}ForecastTimeSteps')
+
+            times = [datetime.datetime.strptime(i.text, '%Y-%m-%dT%H:%M:%S.%fZ') for i in node]
+                    
+            nodes = kml.getroot().find('{http://www.opengis.net/kml/2.2}Document').findall('{http://www.opengis.net/kml/2.2}Placemark')
+            
+            self._stations = [Station(node, times) for node in nodes]
+        else:
+            logger.debug('Use cached stations...')
         return self._stations
 
     def get_nearest_station(self, lat, lon):
--- weathertour.py
+++ weathertour.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 __author__  = 'Konstantin Weddige'
-__version__ = '0.1'
+__version__ = '0.2'
 from argparse import ArgumentParser
 import gpxpy
 import logging
@@ -9,13 +9,17 @@ import datetime
 from dwd.client import DWDClient
 from util import haversine
 
-def follow_tour(points, time=None):
+
+logger = logging.getLogger(__name__)
+
+
+def follow_tour(points, start_time=None):
     last_station = None
     last_point = None
     distance = 0
     duration = datetime.timedelta()
-    if not time:
-        time = datetime.datetime.now()
+    if not start_time:
+        start_time = datetime.datetime.now()
     for point in points:
         if last_point:
             distance += haversine(last_point.latitude, last_point.longitude, point.latitude, point.longitude)
@@ -24,13 +28,25 @@ def follow_tour(points, time=None):
         nearest_station = dwd.get_nearest_station(point.latitude, point.longitude)
         if not last_station == nearest_station:
             last_station = nearest_station
-            time += duration
+            
+            time = start_time + duration
+            fc_time = min((abs((t-time).total_seconds()), t) for t in nearest_station.forecast)[1]
+            fc = nearest_station.forecast[fc_time]
+            
             print('{0:.0f}km\t{1}\t{2} ({3:.2f}km)'.format(distance, duration, last_station, last_station.distance(point.latitude, point.longitude)))
+            
+            #print(fc)
+            
+            print('\t{0}'.format(fc_time))
+            print('\tWind\t\t{0}m/s ({1}°)'.format(fc['FF'], fc['DD']))
+            print('\tTemperature\t{0:.1f}°C'.format(float(fc['TTT']) - 272.15))
+            print('\tCloud cover\t{0}%'.format(fc['N']))
+            print('\tPrecipitation\t{0}l/m²'.format(fc['RR1c']))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--version', action='version', version='%(prog)s {0}'.format(__version__))
-    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--refresh', action='store_true')
     parser.add_argument('--log', default='ERROR')
     parser.add_argument('route', help='GPX file')
     args = parser.parse_args()
@@ -38,16 +54,19 @@ if __name__ == '__main__':
     logging.basicConfig(level=getattr(logging, args.log))
 
     dwd = DWDClient()
+    
+    if args.refresh:
+        dwd.refresh()
 
     with open(args.route, 'r') as f:
         gpx = gpxpy.parse(f)
 
     if gpx.routes:
         for route in gpx.routes:
-            logging.info('Start route')
+            logger.info('Start route')
             follow_tour(route.points)
     else:
         for track in gpx.tracks:
             for segment in track.segments:
-                logging.info('Start segment')
+                logger.info('Start segment')
                 follow_tour(segment.points)
